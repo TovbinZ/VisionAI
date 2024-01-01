5de4ec86c61a6c50504bc4e876eb77095fbfc30e
ZevT88 <65665740+ZevT88@users.noreply.github.com>
2024-01-01 16:06:07 +0200

    init

---
+++ main.py
@@ -0,0 +1,39 @@
+import gradio as gr
+from PIL import Image
+import shutil
+from vision import Single
+from vision import Multiple
+
+
+
+
+def Main(image,Question,QuestionType,Tokens, IsConsice):
+
+	source = "/Users/zevtovbin/Desktop/GradioVision/image1.png"
+	destination = "/Users/zevtovbin/Desktop/GradioVision/image2.png"
+	shutil.copyfile(source, destination)
+
+	source2 = image
+	destination2 = "/Users/zevtovbin/Desktop/GradioVision/image1.png"
+	shutil.copyfile(source2,destination2)
+
+
+	if QuestionType == "Single":
+		result = Single(Question,IsConsice,Tokens)
+	else:
+		result = Multiple(Question,IsConsice,Tokens)
+
+	return result
+
+
+
+inputs = [gr.Image(source="webcam",type='filepath'),gr.Text(),gr.Radio(['Single','Multiple'],value='Single'), gr.Slider(150,350), gr.Radio([True,False],value=False)]
+
+
+App = gr.Interface(fn=Main, inputs=inputs, outputs="text")
+
+
+
+App.launch()
+
+
---
+++ vision.py
@@ -0,0 +1,117 @@
+from openai import OpenAI
+import base64
+
+
+
+client = OpenAI()
+OpenAI.api_key = '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'
+
+
+
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+
+
+def Multiple(Question, IsConsice, Tokens):
+
+
+
+    image_path1 = "/Users/zevtovbin/Desktop/GradioVision/image1.png"
+    image_path2 = "/Users/zevtovbin/Desktop/GradioVision/image2.png"
+
+
+    base64_image1= encode_image(image_path1)
+    base64_image2= encode_image(image_path2)
+
+
+
+
+    if IsConsice:
+        consice = "Answer in a short and consice maner."
+    else:
+        consice = ""
+
+
+    response = client.chat.completions.create(
+        model="gpt-4-vision-preview",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": f"data:image/png;base64,{base64_image2}",
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": f"data:image/png;base64,{base64_image1}",
+                    },
+                    {
+                        "type": "text",
+                        "text": f"{Question} {consice}",
+                    },
+                ],
+            }
+        ],
+        max_tokens=Tokens,
+    )
+
+
+    return (response.choices[0].message.content)
+
+
+def Single(Question, IsConsice, Tokens):
+
+
+
+    image_path1 = "/Users/zevtovbin/Desktop/GradioVision/image1.png"
+    image_path2 = "/Users/zevtovbin/Desktop/GradioVision/image2.png"
+
+
+    base64_image1= encode_image(image_path1)
+    base64_image2= encode_image(image_path2)
+
+
+
+
+    if IsConsice:
+        consice = "Answer in a short and consice maner."
+    else:
+        consice = ""
+
+
+    response = client.chat.completions.create(
+        model="gpt-4-vision-preview",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": f"data:image/png;base64,{base64_image1}",
+                    },
+                    {
+                        "type": "text",
+                        "text": f"{Question} {consice}",
+                    },
+                ],
+            }
+        ],
+        max_tokens=Tokens,
+    )
+
+    return (response.choices[0].message.content)
+
+
+
+
+
+
+
+
+
+
+
